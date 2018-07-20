4a5cc73f465defec3d5b2b4e85539d30f3c34934
陈立信 <Ethan@EthanMacBookAir.local>
2018-07-20 16:10:21 +0800

    Internet connect enemy

--- Supernova/Assets/Scripts/PlayerMove.cs
+++ Supernova/Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+	public GameObject tcpHolder;
+	private TcpClient tcp;
+
 	public iTween.EaseType easeType;
 	public KeyCode up;
 	public KeyCode down;
@@ -42,73 +46,110 @@ public class PlayerMove : MonoBehaviour
 	Hashtable rightArgs2 = iTween.Hash("x", 0.5, "delay", 0.1, "y", -0.25, "time", 0.1, "easetype", iTween.EaseType.easeInOutCubic);
 
 	// Use this for initialization
-	void Start()
-	{
 
+	public void MoveUp()
+	{
+		isMoving = true;
+		faceDown = false;
+		faceUp = true;
+		faceLeft = false;
+		faceRight = false;
+
+		iTween.MoveBy(this.gameObject, upArgs1);
+		iTween.MoveBy(this.gameObject, upArgs2);
 	}
 
+	public void MoveDown()
+	{
+		isMoving = true;
+		faceDown = true;
+		faceUp = false;
+		faceLeft = false;
+		faceRight = false;
+
+		iTween.MoveBy(this.gameObject, downArgs1);
+		iTween.MoveBy(this.gameObject, downArgs2);
+	}
+	public void MoveLeft()
+	{
+		isMoving = true;
+		faceDown = false;
+		faceUp = false;
+		faceLeft = true;
+		faceRight = false;
+
+		iTween.MoveBy(this.gameObject, leftArgs1);
+		iTween.MoveBy(this.gameObject, leftArgs2);
+	}
+	public void MoveRight()
+	{
+		isMoving = true;
+		faceDown = false;
+		faceUp = false;
+		faceLeft = false;
+		faceRight = true;
+
+		iTween.MoveBy(this.gameObject, rightArgs1);
+		iTween.MoveBy(this.gameObject, rightArgs2);
+	}
+
+	public void FixPosition()
+	{
+		fixPositionX = Mathf.Floor(this.transform.position.x) + 0.5f;
+		fixPositionY = Mathf.Floor(this.transform.position.y) + 0.5f;
+		this.transform.position = new Vector2(fixPositionX, fixPositionY);
+	}
+
+	public void StatusCheck()
+	{
+		nowTime += Time.deltaTime;
+		if (nowTime >= timer)
+		{
+			nowTime = 0;
+			isMoving = false;
+		}
+	}
+
+	void Start()
+	{
+		tcp = tcpHolder.GetComponent<TcpClient>();
+	}
+
+
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
 		if (!isMoving)
 		{
 			if (Input.GetKey(up) && !isMoving)
 			{
-				isMoving = true;
-				faceDown = false;
-				faceUp = true;
-				faceLeft = false;
-				faceRight = false;
-
-				iTween.MoveBy(this.gameObject, upArgs1);
-				iTween.MoveBy(this.gameObject, upArgs2);
+				MoveUp();
+				tcp.SendSelfCommand("W");
 			}
-			if (Input.GetKey(down) && !isMoving)
+			else if (Input.GetKey(down) && !isMoving)
 			{
-				isMoving = true;
-				faceDown = true;
-				faceUp = false;
-				faceLeft = false;
-				faceRight = false;
-
-				iTween.MoveBy(this.gameObject, downArgs1);
-				iTween.MoveBy(this.gameObject, downArgs2);
+				MoveDown();
+				tcp.SendSelfCommand("S");
 			}
-			if (Input.GetKey(left) && !isMoving)
+			else if (Input.GetKey(left) && !isMoving)
 			{
-				isMoving = true;
-				faceDown = false;
-				faceUp = false;
-				faceLeft = true;
-				faceRight = false;
-
-				iTween.MoveBy(this.gameObject, leftArgs1);
-				iTween.MoveBy(this.gameObject, leftArgs2);
+				MoveLeft();
+				tcp.SendSelfCommand("A");
 			}
-			if (Input.GetKey(right) && !isMoving)
+			else if (Input.GetKey(right) && !isMoving)
 			{
-				isMoving = true;
-				faceDown = false;
-				faceUp = false;
-				faceLeft = false;
-				faceRight = true;
-
-				iTween.MoveBy(this.gameObject, rightArgs1);
-				iTween.MoveBy(this.gameObject, rightArgs2);
+				MoveRight();
+				tcp.SendSelfCommand("D");
+			}else
+			{
+				tcp.SendSelfCommand(" ");
 			}
 
-			fixPositionX = Mathf.Floor(this.transform.position.x) + 0.5f;
-			fixPositionY = Mathf.Floor(this.transform.position.y) + 0.5f;
-			this.transform.position = new Vector2(fixPositionX, fixPositionY);
+			FixPosition();
 		}
 		else
 		{
-			nowTime += Time.deltaTime;
-			if (nowTime >= timer)
-			{
-				nowTime = 0;
-				isMoving = false;
-			}
+			StatusCheck();
 		}
 
 	}
---
+++ Supernova/Assets/Scripts/TcpClient.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine.UI;
+
+public class TcpClient : MonoBehaviour 
+{	
+	public string targetIP;
+	public int port;
+	Socket serverSocket;
+	IPAddress ip;
+	IPEndPoint ipEnd;
+	int msgLen = 1024;
+	string recvStr;
+	byte[] recvData = new byte[1024];
+	byte[] sendData = new byte[1024];
+	int recvLen;
+	Thread connectThread;
+	public GameObject player2;
+	private EnemyMove em;
+
+
+	void InitSocket()
+	{
+		ip = IPAddress.Parse(targetIP);
+		ipEnd = new IPEndPoint(ip, port);
+		connectThread = new Thread(StartTCPConnect);
+		connectThread.Start();
+	}
+
+	void StartTCPConnect()
+	{
+		serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		Debug.Log("Before Connect");
+		serverSocket.Connect(ipEnd);
+		Debug.Log("Connecting success");
+		while(true)
+		{
+			recvData = new byte[1024];
+			recvLen = serverSocket.Receive(recvData);
+			if(recvLen == 0)
+			{
+				continue;
+			}
+			recvStr = Encoding.ASCII.GetString(recvData,0,1);
+			Debug.Log("Rcvd From Server: " + recvStr + "END");
+			Debug.Log("END2");
+			em.SetCurrentCommand(recvStr);
+			
+		}
+	}
+
+	void SocketQuit()
+	{
+		if(connectThread != null)
+		{
+			connectThread.Interrupt();
+			connectThread.Abort();
+		}
+
+		if(serverSocket != null)
+		{
+			serverSocket.Close();
+		}
+		Debug.Log("Client Quit");
+	}
+
+	void Start()
+	{
+		InitSocket();
+		em = player2.GetComponent<EnemyMove>();
+	}
+
+	public void SendSelfCommand(string str)
+	{	
+		byte[] commandSelf = new byte[msgLen];
+		commandSelf = Encoding.ASCII.GetBytes(str);
+		serverSocket.Send(commandSelf, commandSelf.Length, SocketFlags.None);
+	}
+
+
+	void OnDisable()
+	{
+		SocketQuit();	
+	}
+
+
+}
