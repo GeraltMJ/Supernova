e0d7292b523c9291554db94b94b3c1f3b9eb5255
ethankidding <chenl19@rpi.edu>
2018-07-27 18:28:54 +0800

    check

--- Supernova/Assets/NetWorkTest/NetWorkMove.cs
+++ Supernova/Assets/NetWorkTest/NetWorkMove.cs
@@ -5,90 +5,49 @@ using UnityEngine;
 public class NetWorkMove : MonoBehaviour {
 	
 	// Update is called once per frame
-	private string currentCommand;
-	public GameObject NetworkManager;
-	private TcpClient tcpClient;
-
-	public void SetCurrentCommand(string command)
-	{
-		currentCommand = command;
-	}
-	void EnemyMove()
+	void MoveDown()
 	{
 		Vector3 oldPosition = transform.position;
-		if(currentCommand == "W")
-		{
-			oldPosition.y += 1;
-		}
-		else if(currentCommand == "S")
-		{
-			oldPosition.y -= 1;
-		}
-		else if(currentCommand == "A")
-		{
-			oldPosition.x -= 1;
-		}
-		else if(currentCommand == "D")
-		{
-			oldPosition.x += 1;
-		}
-		currentCommand = "";
+		oldPosition.y -= 1;
+		transform.position = oldPosition;
+	}
+	void MoveLeft()
+	{
+		Vector3 oldPosition = transform.position;
+		oldPosition.x -= 1;
+		transform.position = oldPosition;
+	}
+	void MoveRight()
+	{
+		Vector3 oldPosition = transform.position;
+		oldPosition.x += 1;
+		transform.position = oldPosition;
+	}
+	void MoveUp()
+	{
+		Vector3 oldPosition = transform.position;
+		oldPosition.y += 1;
 		transform.position = oldPosition;
 	}
 
-	void MoveControl()
+	
+	void FixedUpdate()
 	{
-		Vector3 oldPosition = transform.position;
 		if(Input.GetKeyDown(KeyCode.W))
 		{
-			tcpClient.SendSelfCommand("W");
-			oldPosition.y += 1;
+			MoveUp();
 		}
-		else if(Input.GetKeyDown(KeyCode.S))
+		if(Input.GetKeyDown(KeyCode.S))
 		{
-			tcpClient.SendSelfCommand("S");
-			oldPosition.y -= 1;
+			MoveDown();
 		}
-		else if(Input.GetKeyDown(KeyCode.A))
+		if(Input.GetKeyDown(KeyCode.A))
 		{
-			tcpClient.SendSelfCommand("A");
-			oldPosition.x -= 1;
+			MoveLeft();
 		}
-		else if(Input.GetKeyDown(KeyCode.D))
+		if(Input.GetKeyDown(KeyCode.D))
 		{
-			tcpClient.SendSelfCommand("D");
-			oldPosition.x += 1;
+			MoveRight();
 		}
-		transform.position = oldPosition;
-	}
-
-	void Start()
-	{
-		tcpClient = NetworkManager.GetComponent<TcpClient>();
-	}
-	void FixedUpdate () {
-		
-		if(gameObject.name == "Player1")
-		{
-			if(PlayerStatusControl._instance.isPlayer1)
-			{
-				MoveControl();
-			}
-			else
-			{
-				EnemyMove();
-			}
-		}
-		else if(gameObject.name == "Player2")
-		{
-			if(!PlayerStatusControl._instance.isPlayer1)
-			{
-				MoveControl();
-			}else
-			{
-				EnemyMove();
-			}
-		}
-
 	}
 }
