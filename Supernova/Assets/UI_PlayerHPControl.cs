d3915fcdb2c1bb16f0778eb2e82867509cb8502c
GeraltMJ <liulihang.geraltliu@gmail.com>
2018-08-14 14:09:28 +0800

    Add UI

---
+++ Supernova/Assets/UI_PlayerHPControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PlayerHPControl : MonoBehaviour {
+
+	public Slider slider;
+	public Text currentHPUI;
+	public Text maxHPUI;
+	public float currentHP = 18.0f;
+	public float maxHP = 23.0f;
+
+	// Use this for initialization
+	void Start () {
+		currentHPUI.text = currentHP.ToString();
+		maxHPUI.text = maxHP.ToString();
+		slider.value = currentHP / maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
